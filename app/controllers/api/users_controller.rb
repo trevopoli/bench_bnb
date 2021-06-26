87b57f7debd6fa205c66438567199854987132ad
trevopoli <trevopoli@gmail.com>
2021-06-26 15:55:38 -0700

    users controller implemented

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,21 @@ class Api::UsersController < ApplicationController
 
     def create
 
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            render "api/users/show"
+        else
+            render json: @users.errors.full_messages, status: 422
+        end
     end
-    
+
+
+    private
+
+    def under_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
