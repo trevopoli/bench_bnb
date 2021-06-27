dc78a63b5e12c5bbb59163683d43a22f1b1a6ed5
trevopoli <trevopoli@gmail.com>
2021-06-27 14:39:00 -0700

    phase 1 complete

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 import * as SessionAPIUtil from "./util/session_api_util";
 
 window.signup = SessionAPIUtil.signup;
@@ -7,6 +8,11 @@ window.logout = SessionAPIUtil.logout;
 window.login = SessionAPIUtil.login;
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
